edcc2cea939c438395739de7c61c80c4c91b4f5c
durongze <durongze@qq.com>
2017-11-21 21:50:14 +0800

    修改：     git001.sh

--- git/git001.sh
+++ git/git001.sh
@@ -42,7 +42,7 @@ git ls-tree 0593
 git show -s --pretty-raw 2364
 
 
-#References(Branch  Remote Head)
+#References(Branches  RemoteBranches HEAD)
 ls -l .git/HEAD
 cat .git/HEAD
 ls -l .git/refs/
@@ -51,6 +51,25 @@ cat .git/refs/heads/master #HEAD 对应的master
 find .git/objects/ -type f #查看和哪个master对应
 git cat-file -t cb02
 
+#tag
+ls -l .git/refs/tags/
+git tag v1.0
+cat .git/refs/tags/v1.0
+git cat-file -t 9e7b
+find .git/objects/ -type f | wc -l
+#以上tag非对象
+
+git tag -a milestone1.0 -m "This is the first stable version"
+find .git/objects/ -type f | wc -l
+#以上tag是个对象
+ls -l .git/refs/tags/
+cat .git/refs/tags/milestone1.0
+
+#取出归档版本代码
+git archive --format=tar --prefix=ruby/ v1.0 | gzip > /tmp/ruby1.0.tar.gz
+
+#通过tag取代码
+git checkout v0.1 ===> .git/refs/tags/v0.1 ===> tag:0c819c  ===> commit:a11bef
 
 
 
