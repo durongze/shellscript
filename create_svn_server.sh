5b1d17f46dcc4aa7e854b6138dc11476004d59a6
durongze <durongze@qq.com>
2018-03-10 17:42:58 +0800

    Create create_svn_server.sh

---
+++ create_svn_server.sh
@@ -0,0 +1,14 @@
+1. svnadmin create /home/git/code/svn/svnrepos
+2. vi conf/authz 
+   [\]
+   git = rw
+3. vi conf/passwd
+   git = secret
+4. vi conf/svnserve.conf
+   anon-access = read
+   auth-access = write
+   password-db = passwd
+   authz-db = authz
+   realm = My First Repository
+5. svnserve -d -r /home/git/code/svn/svnrepos
+6. sudo iptables -I INPUT -p tcp --dport 3690 -j ACCEPT
