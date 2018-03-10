4e7e8ea72b7d0fd6936e38f188f3dc1171b06f48
durongze <durongze@qq.com>
2018-03-10 22:14:45 +0800

    Update find_ldd_file.sh

--- find_ldd_file.sh
+++ find_ldd_file.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #得到某文件所依赖的所有文件的绝对路径
-ldd demo | sed -e 's/^.*=>//g' | sed -e 's/(.*)//g'
+方法1 ldd demo | sed -e 's/^.*=>//g' | sed -e 's/(.*)//g'
+方法2 ldd demo | cut -f2 -d '>' | cut -f1 -d '('
 #去掉ldd命令结果中的前缀
 sed -e "s#^.*=>##g" demo.ldd 
 #去掉ldd命令结果中的后缀
