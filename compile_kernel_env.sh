52b0c7b6fec70e342510cf1083034ddc7ca3e5d8
durongze <durongze@qq.com>
2018-03-17 01:12:29 +0800

    Create compile_kernel_env.sh

---
+++ compile_kernel_env.sh
@@ -0,0 +1,9 @@
+
+
+
+
+sudo  yum install glibc-2.17-55.el7.i686.rpm  
+sudo  yum install libstdc++-4.8.2-16.el7.i686.rpm 
+sudo  yum install zlib-1.2.7-13.el7.i686.rpm 
+sudo yum install bc-1.06.95-13.el7.x86_64.rpm 
+cp  arch/arm/configs/hi3536c_full_defconfig .config 
