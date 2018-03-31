bf8221747963a7bc12b9a7ee9671d26e60c0bfb9
durongze <durongze@qq.com>
2018-03-31 09:09:14 +0800

    Update compile_kernel_env.sh

--- compile_kernel_env.sh
+++ compile_kernel_env.sh
@@ -7,3 +7,8 @@ sudo  yum install libstdc++-4.8.2-16.el7.i686.rpm
 sudo  yum install zlib-1.2.7-13.el7.i686.rpm 
 sudo yum install bc-1.06.95-13.el7.x86_64.rpm 
 cp  arch/arm/configs/hi3536c_full_defconfig .config 
+
+
+yum groupinstall "Development Tools"
+
+yum install gcc make ncurses ncurses-devel perl  kernel-devel
