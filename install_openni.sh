b1185ffb85cf8abdc38e1fbf0482a59cba650c9c
durongze <durongze@qq.com>
2018-05-21 14:05:55 +0800

    Update install_openni.sh

--- install_openni.sh
+++ install_openni.sh
@@ -2,6 +2,11 @@
 
 sudo yum install usbutils-007-4.el7.x86_64.rpm 
 
+function remove_tmp_dir()
+{
+    ls -F | grep "/$" | xargs rm -rf
+}
+
 function install_doxygen1814()
 {
     echo -e "\033[32m $FUNCNAME \033[0m"
@@ -125,6 +130,75 @@ function install_mesa1802()
     make install   || { echo "$FUNCNAME $LINENO failed,${FUNCNAME[1]} ${BASH_LINENO[1]} "; exit 1; }
     popd    
 }
+
+function AutoInstall()
+{
+    SrcDir=$1
+    DstDir=$2
+    CurFlags=$3
+    pushd $FileDir
+        if [ -f CMakeLists.txt ];then
+            mkdir build  || { echo "$FUNCNAME $LINENO failed,${FUNCNAME[1]} ${BASH_LINENO[1]} "; exit 1; }
+            pushd build
+            export CXXFLAGS="-fPIC" && cmake  -DCMAKE_INSTALL_PREFIX=$DstDir/$SrcDir ..
+            make  || { echo "$FUNCNAME $LINENO failed,${FUNCNAME[1]} ${BASH_LINENO[1]} "; exit 1; }
+            make install  || { echo "$FUNCNAME $LINENO failed,${FUNCNAME[1]} ${BASH_LINENO[1]} "; exit 1; }
+            popd
+        elif [ -f configure ];then
+            mkdir build
+            pushd build
+            ../configure --prefix=$DstDir/$SrcDir $CurFlags  || { echo "$FUNCNAME $LINENO failed,${FUNCNAME[1]} ${BASH_LINENO[1]} "; exit 1; }
+            make   || { echo "$FUNCNAME $LINENO failed,${FUNCNAME[1]} ${BASH_LINENO[1]} "; exit 1; }
+            make install   || { echo "$FUNCNAME $LINENO failed,${FUNCNAME[1]} ${BASH_LINENO[1]} "; exit 1; }
+            popd
+        else
+            echo -e "\033[31m Install $SrcDir Fail !!! \033[0m"
+            exit 1
+        fi
+    popd
+}
+
+function TarXFFile()
+{
+    SrcDir=$1
+    DstDir=$2
+    CurFlags=$3
+    pushd ./
+        SrcFileList=$SrcDir
+        for file in $SrcFileList
+        do
+            FileDir=$(tar -tf $file | cut -f1 -d'/' | uniq)
+            echo -e "\033[32mFile:\033[0m$file \033[32mDir:\033[0m$FileDir"
+            tar xf $file
+            AutoInstall $FileDir $DstDir $CurFlags
+        done
+    popd
+}
+
+function SetMesaEnv()
+{
+    fileList="cmake360 glib2482 python2714 OpenBLAS gobject1380 gperf304 udev174 libusb1022 graphviz2401 doxygen1814 freeglut300 boost xtrans-1.2.7 xproto-7.0.23 xcb-proto-1.13 libxcb-1.13 libxshmfence-1.3 expat-2.2.5 libpciaccess-0.10.3 libdrm-2.4.92 
+llvm -6.0.0.src libxshmfence-1.3 expat-2.2.5 mesa-18.0.2"
+    for tmpFile in $fileList
+    do
+        TMP_FILE_HOME=${HOME}/opt/$tmpFile
+        export C_INCLUDE_PATH=$TMP_FILE_HOME/include:$C_INCLUDE_PATH
+        export CPLUS_INCLUDE_PATH=$TMP_FILE_HOME/include:$CPLUS_INCLUDE_PATH
+        export LIBRARY_PATH=$TMP_FILE_HOME/lib:$LIBRARY_PATH
+        export LD_LIBRARY_PATH=$TMP_FILE_HOME/lib:$LD_LIBRARY_PATH
+        export PKG_CONFIG_PATH=$TMP_FILE_HOME/lib/pkgconfig/:$PKG_CONFIG_PATH
+        export PATH=$TMP_FILE_HOME/bin:$TMP_FILE_HOME/sbin:$PATH
+    done
+}
+
+#TarXFFile "xcb-proto-1.13.tar.bz2" ~/opt ""
+#TarXFFile "libxcb-1.13.tar.bz2" ~/opt  ""
+#TarXFFile "libpciaccess-0.10.3.tar.bz2" ~/opt  ""
+#TarXFFile "libdrm-2.4.92.tar.gz" ~/opt  " --enable-intel --enable-libkms "
+#TarXFFile "llvm-6.0.0.src.tar.xz" ~/opt  ""
+#TarXFFile "libxshmfence-1.3.tar.bz2" ~/opt  ""
+#TarXFFile "expat-2.2.5.tar.bz2" ~/opt  ""
+#TarXFFile "mesa-18.0.2.tar.gz" ~/opt  ""
 
 function ubuntu_cfg_install()
 { 
