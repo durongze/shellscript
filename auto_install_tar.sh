25014d859ded7bb4bf0458d4e2ce8904be3b4315
durongze <durongze@qq.com>
2018-06-11 20:33:55 +0800

    Update auto_install_tar.sh

--- auto_install_tar.sh
+++ auto_install_tar.sh
@@ -62,16 +62,38 @@ function ModifyWxGTK()
     find wxGTK-2.8.12/ -iname "*.xbm" | xargs -I {} sed -i 's/0x/char(0x/g' {}
     find wxGTK-2.8.12/ -iname "*.xbm" | xargs -I {} sed -i 's/,/),/g' {}
     find wxGTK-2.8.12/ -iname "*.xbm" | xargs -I {} sed -i 's/}/)}/g' {}
+    sed -i 's#DEFAULT_wxUSE_STL=no#DEFAULT_wxUSE_STL=yes#g' wxGTK-2.8.12/configure
+    sed -i 's#DEFAULT_wxUSE_NOTEBOOK=no#DEFAULT_wxUSE_NOTEBOOK=yes#g' wxGTK-2.8.12/configure
 }
+
+function GenInputHeader()
+{
+    cd /usr/include/X11/extensions && sudo ln -s XI.h XInput.h
+}
+
 #TarXFFile "glib-2.56.0.tar.xz" ~/opt ""
 #TarXFFile "atk-1.29.92.tar.gz" ~/opt ""
 #TarXFFile "tiff-4.0.9.tar.gz" ~/opt " --shared "
 #TarXFFile "libjpeg-turbo-1.5.3.tar.gz" ~/opt ""
 #TarXFFile "gdk-pixbuf-2.30.8.tar.xz" ~/opt ""
 #TarXFFile "gtk+-2.24.32.tar.xz" ~/opt ""
-#TarXFFile "wxGTK-2.8.12.tar.gz" ~/opt " --enable-unicode "
+#TarXFFile "wxGTK-2.8.12.tar.gz" ~/opt " --enable-unicode --enable-stl --enable-gui --enable-shared --enable-msgdlg "
 #TarXFFile "libupnp-1.6.21.tar.bz2" ~/opt ""
 #"cryptopp565.zip"
 #make install PREFIX=${HOME}/opt/cryptopp565
 #TarXFFile "zlib-1.2.11.tar.gz" ~/opt ""
 TarXFFile "aMule-2.3.2.tar.xz" ~/opt " --enable-amule-daemon --enable-amulecmd --enable-webserver --enable-amule-gui --enable-alc --enable-alcc --enable-fileview --enable-plasmamule "
+
+
+#TarXFFile "xcb-proto-1.13.tar.bz2" ~/opt ""
+#TarXFFile "libxcb-1.13.tar.bz2" ~/opt ""
+#TarXFFile "glib-2.57.1.tar.xz" ~/opt ""
+#TarXFFile "gobject-introspection-1.56.1.tar.bz2" ~/opt ""
+#TarXFFile "gperf-3.1.tar.gz" ~/opt ""
+#TarXFFile "udev-181.tar.gz" ~/opt " --disable-keymap "
+#TarXFFile "libusb-1.0.22.tar.bz2" ~/opt ""
+#TarXFFile "usbutils-007.tar.gz" ~/opt ""
+#TarXFFile "graphviz.tar.gz" ~/opt ""
+#TarXFFile "doxygen-1.8.14.src.tar.gz" ~/opt ""
+#X11/extensions/XInput.h: No such file or directory #GenInputHeader
+#TarXFFile "freeglut-3.0.0.tar.gz" ~/opt ""
