d8ad3c66b215eb99317a5e73a5bddf4e38d7443c
durongze <durongze@qq.com>
2018-10-28 21:29:17 +0800

    django

---
+++ django/http/http/wsgi.py
@@ -0,0 +1,28 @@
+"""
+WSGI config for http project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+######import os
+######
+######from django.core.wsgi import get_wsgi_application
+######
+######os.environ.setdefault("DJANGO_SETTINGS_MODULE", "http.settings")
+######
+######application = get_wsgi_application()
+######
+import os 
+from os.path import join,dirname,abspath 
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "http.settings") 
+PROJECT_DIR = dirname(dirname(abspath(__file__))) 
+
+import sys 
+sys.path.insert(0,PROJECT_DIR) 
+
+from django.core.wsgi import get_wsgi_application 
+application = get_wsgi_application()
