6673dbd3cfaf3db4572f668baf6ae7a995dbbb11
durongze <durongze@qq.com>
2019-01-01 19:13:04 +0800

    tensorflow

--- tensorflow.sh
+++ tensorflow.sh
@@ -6,21 +6,33 @@ function AutoInstall()
     DstDir=$2
     CurFlags="$3"
     InstallDir=$(echo $SrcDir | tr -s "." "_")
-    echo "DstDir:$DstDir InstallDir:$InstallDir CurFlags:$CurFlags"
     pushd $SrcDir
     if [ -f CMakeLists.txt ];then
         mkdir dyzbuild  
         pushd dyzbuild
+        echo -e "\033[32m export CXXFLAGS=\"-fPIC\" && cmake  -DCMAKE_INSTALL_PREFIX=$DstDir/$InstallDir .. \033[0m"
         export CXXFLAGS="-fPIC" && cmake  -DCMAKE_INSTALL_PREFIX=$DstDir/$InstallDir ..
         make  || { echo "$FUNCNAME $LINENO failed,${FUNCNAME[1]} ${BASH_LINENO[1]} "; exit 1; }
         make install  || { echo "$FUNCNAME $LINENO failed,${FUNCNAME[1]} ${BASH_LINENO[1]} "; exit 1; }
         popd
     elif [ -f setup.py ];then
         sudo python setup.py install 
+    elif [ -f autogen.sh ];then
+        ./autogen.sh
+        make distclean
+        mkdir dyzbuild
+        pushd dyzbuild
+        dos2unix ../configure && export CXXFLAGS="-fPIC"
+        echo -e "\033[32m ../configure --prefix=$DstDir/$InstallDir $CurFlags \033[0m" 
+        ../configure --prefix=$DstDir/$InstallDir $CurFlags  || { echo "$FUNCNAME $LINENO failed,${FUNCNAME[1]} ${BASH_LINENO[1]} "; exit 1; }
+        make   || { echo "$FUNCNAME $LINENO failed,${FUNCNAME[1]} ${BASH_LINENO[1]} "; exit 1; }
+        make install   || { echo "$FUNCNAME $LINENO failed,${FUNCNAME[1]} ${BASH_LINENO[1]} "; exit 1; }
+        popd
     elif [ -f configure ];then
         mkdir dyzbuild
         pushd dyzbuild
         dos2unix ../configure && export CXXFLAGS="-fPIC"
+        echo -e "\033[32m ../configure --prefix=$DstDir/$InstallDir $CurFlags \033[0m" 
         ../configure --prefix=$DstDir/$InstallDir $CurFlags  || { echo "$FUNCNAME $LINENO failed,${FUNCNAME[1]} ${BASH_LINENO[1]} "; exit 1; }
         make   || { echo "$FUNCNAME $LINENO failed,${FUNCNAME[1]} ${BASH_LINENO[1]} "; exit 1; }
         make install   || { echo "$FUNCNAME $LINENO failed,${FUNCNAME[1]} ${BASH_LINENO[1]} "; exit 1; }
@@ -160,6 +172,14 @@ function InstallQGisDep()
     sudo apt-get install libpqxx-doc 
 }
 
+function ImagingModify()
+{
+    JPEG_ROOT = libinclude("/home/duyongze/opt/jpeg-9c")
+    ZLIB_ROOT = libinclude("/home/duyongze/opt/zlib-1_2_11")
+    TIFF_ROOT = libinclude("/home/duyongze/opt/libffi-3_2_1")
+    FREETYPE_ROOT = libinclude("/home/duyongze/opt/freetype-2_9")
+    LCMS_ROOT = libinclude("/home/duyongze/opt/lcms2-2_9")
+}
 #GenFileNameVar "$(ls *.tar.* *.zip)"
 #GenEnvVar
 #TarXFFile "setuptools-40.6.2.zip" "" ""
@@ -181,13 +201,13 @@ function InstallQGisDep()
 #TarXFFile "Werkzeug-0.14.1.tar.gz" "" ""
 #sudo apt-get install python-dev
 #TarXFFile "grpcio-1.17.1.tar.gz" "" ""
-########TarXFFile "tensorboard-1.12.0.zip" "" ""
+#########TarXFFile "tensorboard-1.12.0.zip" "" ""
 #TarXFFile "pbr-5.1.1.tar.gz" "" ""
 #TarXFFile "mock-2.0.0.tar.gz" "" ""
 #TarXFFile "pycparser-2.19.tar.gz" "" ""
 #TarXFFile "libffi-3.2.1.tar.gz" "${HOME}/opt" ""
 #TarXFFile "cffi-1.11.5.tar.gz" "" "" 
-## sudo apt-get install  libssl-dev
+#sudo apt-get install  libssl-dev
 #TarXFFile "ipaddress-1.0.22.tar.gz" "" "" 
 #TarXFFile "asn1crypto-0.24.0.tar.gz" "" ""
 #TarXFFile "idna-2.8.tar.gz" "" ""
@@ -214,15 +234,15 @@ function InstallQGisDep()
 #TarXFFile "python-dateutil-2.7.5.tar.gz" "" ""
 #TarXFFile "pyparsing-2.3.0.tar.gz" "" ""
 #TarXFFile "cycler-0.10.0.tar.gz" "" ""
-#TarXFFile "matplotlib-2.2.3.tar.gz" "" ""
+#TarXFFile "matplotlib-2.2.3.tar.gz" "" "" #sudo apt-get install libfreetype6-dev
 #TarXFFile "cairocffi-0.9.0.tar.gz" "" ""
 #TarXFFile "packaging-18.0.tar.gz" "" "" 
 #TarXFFile "scikit-build-0.8.1.tar.gz" "" ""
 #TarXFFile "opencv-python-19.tar.gz" "" ""  ####pip install opencv-python
 #TarXFFile "zlib-1.2.11.tar.gz" "${HOME}/opt" "--shared" 
-#TarXFFile "freetype-2.9.tar.bz2" "${HOME}/opt" " CFLAGS=-fPIC CXXFLAGS=-fPIC --shared "
+#TarXFFile "freetype-2.9.tar.bz2" "${HOME}/opt" " CFLAGS=-fPIC CXXFLAGS=-fPIC --enable-shared "
 #TarXFFile "jpegsrc.v9c.tar.gz" "${HOME}/opt" ""
-#TarXFFile "libpng-1.6.34.tar.gz" "${HOME}/opt" ""
+#TarXFFile "libpng-1.6.34.tar.gz" "${HOME}/opt" "" # move CMakeList.txt dyz.txt
 #TarXFFile "tiff-4.0.9.tar.gz" "${HOME}/opt" ""
-#TarXFFile "Imaging-1.1.7.tar.gz" "" ""  #sudo pip install Pillow-PIL
+TarXFFile "Imaging-1.1.7.tar.gz" "" ""  #sudo pip install Pillow-PIL
 #TarXFFile "lcms2-2.9.zip" "${HOME}/opt" ""
