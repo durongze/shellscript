ae6e1939a33037a0928ded52c58e9ecd82a5d866
durongze <durongze@qq.com>
2020-08-04 16:05:13 +0100

    read xml

--- read_xml.sh
+++ read_xml.sh
@@ -3,13 +3,14 @@ FileName=$1
 Filter=$2
 idx=0
 echo "$FileName" "$Filter"
-grep "${Filter}" ${FileName} | awk -F'=' '{print $3}' | while read V
+grep -n "${Filter}" ${FileName} | awk -F'=' '{print $1 ":" $3}' | while read V
 do 
     idx=$(expr $idx + 1)
-    #echo "V : $V"
-    key=$(echo $"$V" | awk -F'>' '{print $1}')
+    echo "V : $V"
+    line=$(echo $"$V" | awk -F'>' '{print $1}' | cut -f1 -d':')
+    key=$(echo $"$V" | awk -F'>' '{print $1}' | cut -f3 -d':')
     val=$(echo $"$V" | awk -F'>' '{print $2}' | cut -f1 -d'<')
     if [ "$key" != "" ] && [ "$val" != "" ];then
-        echo "idx : $idx, key: $key , val: \"$val\""
+        echo "idx:$idx, line:$line, key:$key, val:\"$val\""
     fi
 done
