82a45e0c30734c229825604bbcb3185af3a52be4
durongze <durongze@qq.com>
2020-10-02 01:48:56 +0800

    Update ffmpeg_app.sh

--- ffmpeg_app.sh
+++ ffmpeg_app.sh
@@ -6,6 +6,8 @@ for /r %%i in (%video_dir%\*.*) do (%ffmpeg_app% -i "%%i" -c copy  -f flv "%%i.f
 @rem for /r %%i in (%video_dir%\*.*) do (%ffmpeg_app% -re -i %%i -c copy  -f hls -bsf:v h264_mp4toannexb %%i.m3u8 )
 @rem for /r %%i in (%video_dir%\*.*) do (%ffmpeg_app% -i "%%i" -c copy "%%i.mp4" )
 
+@rem 获取第7s位置的视频帧
+for /r %%i in (%video_dir%\*.*) do (%ffmpeg_app% -i "%%i" -ss 00:00:7.435 -vframes 1 out.png )
 @rem 除以6=0.1s 一张图 除以60=1s -张图
 for /r %%i in (%video_dir%\*.*) do (%ffmpeg_app% -i "%%i" -vf fps=1/6 thum%%04d.png )
 @rem for /r %%i in (%video_dir%\*.*) do (%ffmpeg_app% -i "%%i" -vf "select='eq(pict_type, PICT_TYPE_I)'" -vsync vfr thum%%04d.png )
