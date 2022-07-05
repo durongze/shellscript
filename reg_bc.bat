890b53648791d4f3191282024f753db16d2d36de
durongze <durongze@qq.com>
2022-07-05 12:01:20 +0800

    Create reg_bc.bat

---
+++ reg_bc.bat
@@ -0,0 +1,9 @@
+echo "reg query start...."
+
+reg query HKEY_CURRENT_USER\SOFTWARE\Scooter" "Software\Beyond" "Compare" "4 /v CacheID
+
+reg delete HKEY_CURRENT_USER\SOFTWARE\Scooter" "Software\Beyond" "Compare" "4 /v CacheID /f
+
+@rem reg add HKEY_CURRENT_USER\SOFTWARE\Scooter" "Software\Beyond" "Compare" "4 /v CacheID /f /t REG_BINARY /d 0 
+
+pause
