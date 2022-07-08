2591190f1fc450bda3d2c64c47f2820e5aef0801
durongze <durongze@qq.com>
2022-07-08 16:28:25 +0800

    Update php_apache_install.sh

--- php_apache_install.sh
+++ php_apache_install.sh
@@ -44,6 +44,43 @@ function GetInstallMethod()
 	echo $Method
 }
 
+function FixApachePkg()
+{
+	ApacheDir="apache_1.3.42"
+
+	ShellName=$(grep "#!/bin/sh" ${ApacheDir}/configure)
+	if [ "$ShellName" != "" ];then
+		echo "sed 's#!/bin/sh#!/bin/bash#g' -i ${ApacheDir}/configure"
+		sed 's#!/bin/sh#!/bin/bash#g' -i ${ApacheDir}/configure
+	fi
+	
+	HeaderFile=$(grep "#include <dlfcn.h>" ${ApacheDir}/src/os/unix/os.c)
+	if [ "$HeaderFile" == "" ];then
+		echo "sed '23a#include <dlfcn.h>' -i ${ApacheDir}/src/os/unix/os.c"
+		sed '23a#include <dlfcn.h>' -i ${ApacheDir}/src/os/unix/os.c
+	fi
+    #src/os/unix/os-inline.c  #inline
+    #src/os/unix/os.h         #inline
+    #src/support/htpasswd.c   #getline
+    #src/support/logresolve.c #getline
+	#+--------------------------------------------------------+
+	#| You now have successfully built and installed the      |
+	#| Apache 1.3 HTTP server. To verify that Apache actually |
+	#| works correctly you now should first check the         |
+	#| (initially created or preserved) configuration files   |
+	#|                                                        |
+	#|   /home/du/opt/apache_1_3_42/conf/httpd.conf
+	#|                                                        |
+	#| and then you should be able to immediately fire up     |
+	#| Apache the first time by running:                      |
+	#|                                                        |
+	#|   /home/du/opt/apache_1_3_42/bin/apachectl start
+	#|                                                        |
+	#| Thanks for using Apache.       The Apache Group        |
+	#|                                http://www.apache.org/  |
+	#+--------------------------------------------------------+
+}
+
 #TarXFFile "xz-5.2.5.tar.gz" "${HOME}/opt"
 #TarXFFile "libxml2-2.9.14.tar.xz" "${HOME}/opt" "" "$(GetInstallMethod "libxml2-2.9.14.tar.xz")"
 #CopyPcFile "libxml2-2.9.14.tar.xz" "${HOME}/opt"
@@ -53,6 +90,7 @@ function GetInstallMethod()
 #TarXFFile "apr-1.7.0.tar.gz" "${HOME}/opt"  "" "$(GetInstallMethod "apr-1.7.0.tar.gz")"
 #TarXFFile "expat-2.4.8.tar.gz" "${HOME}/opt"  "" "$(GetInstallMethod "expat-2.4.8.tar.gz")"
 #TarXFFile "apr-util-1.6.1.tar.gz" "${HOME}/opt"  "--with-apr=${HOME}/opt/apr-1_7_0" "$(GetInstallMethod "apr-util-1.6.1.tar.gz")"
-TarXFFile "apache_1.3.42.tar.gz" "${HOME}/opt"  "" "$(GetInstallMethod "apache_1.3.42.tar.gz")"
-TarXFFile "httpd-2.4.54.tar.gz" "${HOME}/opt" "--with-apr-util=${HOME}/opt/apr-util-1_6_1  --enable-module=most --enable-mods-shared=all --enable-so --enable-include --enable-headers" "$(GetInstallMethod "httpd-2.4.54.tar.gz")" 
-TarXFFile "php-8.1.7.tar.gz" "${HOME}/opt"
+#FixApachePkg
+#TarXFFile "apache_1.3.42.tar.gz" "${HOME}/opt"  "" "$(GetInstallMethod "apache_1.3.42.tar.gz")"
+#TarXFFile "httpd-2.4.54.tar.gz" "${HOME}/opt" "--with-apr=${HOME}/opt/apr-1_7_0 --enable-module=most --enable-mods-shared=all --enable-so --enable-include --enable-headers" "$(GetInstallMethod "httpd-2.4.54.tar.gz")" 
+TarXFFile "php-8.1.7.tar.gz" "${HOME}/opt" "--with-apxs2=/home/du/opt/httpd-2_4_54/bin/apxs" ""
