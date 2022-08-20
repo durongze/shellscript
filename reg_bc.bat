897f50fc01e4813c2dc0d9338fd3cc4d73f8b8bc
durongze <durongze@qq.com>
2022-08-20 10:06:26 +0800

    Update reg_bc.bat

--- reg_bc.bat
+++ reg_bc.bat
@@ -1,3 +1,7 @@
+pushd "C:\Users\%UserName%\AppData\Roaming\Scooter Software\Beyond Compare 4"
+del *  /q
+popd
+
 echo "reg query start...."
 
 reg query HKEY_CURRENT_USER\SOFTWARE\Scooter" "Software\Beyond" "Compare" "4 /v CacheID
