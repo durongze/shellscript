3c3ff8e45a3108dbc7aa7e69eb2e2e5dc020e98d
durongze <durongze@qq.com>
2022-11-28 14:20:01 +0800

    Update fix_opensrc_path.sh

--- fix_opensrc_path.sh
+++ fix_opensrc_path.sh
@@ -4,13 +4,20 @@ function FixOpenSrcPath
 {
     PrefixHomeDir=$1
     TargetHomeDir=$2
+    if [ ! -d $TargetHomeDir ];then
+        mkdir -p $TargetHomeDir
+    fi
     for libDir in $(ls ${PrefixHomeDir})
     do
-        libVer=""
-        if [ -d $libDir/lib ];then
-            libVer=$(ls ${libDir})
-            echo "$libDir -> $libVer"
-            echo ln -sf ${PrefixHomeDir}/${libDir}/${libVer} ${TargetHomeDir}/${libDir}
+        libVerAll=""
+        if [ ! -d $libDir/lib ];then
+            libVerAll=$(ls ${libDir})
+            echo "$libDir -> $libVerAll"
+            for libVer in $libVerAll
+            do
+                echo ln -sf ${PrefixHomeDir}/${libDir}/${libVer} ${TargetHomeDir}/${libDir}
+                break
+            done
         else
             echo "$libDir -> ****************"
             echo ln -sf ${PrefixHomeDir}/${libDir}/${libVer} ${TargetHomeDir}/${libDir}
