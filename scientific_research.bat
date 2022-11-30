53d3c84870c599795131a5f1c6c60111a2a896eb
durongze <durongze@qq.com>
2022-11-30 10:37:33 +0800

    scientific_research

---
+++ scientific_research.bat
@@ -0,0 +1,8 @@
+@rem https://new.qq.com/rain/a/20211117a08zgm00
+pharmacology/biology/chemistry:Autodock Vina、NetMHC、Qvina、Amber、MaterialsStudio、GATK、VASP、Rosetta、Schrödinger、BCFtools、Gromacs、FastQC、DeltaVina、Gaussian
+
+IntegratedCircuit/MicroElectronics:Innovus、Spectre、Genus、Dracula、Virtuoso、Ncsim、PowerSI、Xcelium、PT、DC、VCS、VC、FM、Verdi、OPC Proteus、Tmax2、HSPICE、Spyglass、Starrc、Calibre、Tessent、nmLVS、nmDRC、xACT、xL、xRC
+
+mechanics/thermology/MechanicalEngineering:Abaqus、Autodesk、Bladed、CFX、COMSOL、Fluent、HyperWorks、LS-DYNA、Matlab、Mechanical、MSC Adams、MSC Nastran、StarCCM、SOLIDWORKS、VASP、WRF
+
+ArtificialIntelligence:Pytorch、Mxnet、Tensorflow、Caffe2、Miniconda、Scikit Learn/OpenCV、Pylearn2、Keras
