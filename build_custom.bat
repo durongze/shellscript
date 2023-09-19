1f6809712b835e40d5a41fa24653951530738524
durongze <durongze@qq.com>
2023-09-19 15:27:46 +0800

    Update build_custom.bat

--- build_custom.bat
+++ build_custom.bat
@@ -11,47 +11,160 @@ set VSCMD_DEBUG=2
 @rem see winsdk.bat -> GetWin10SdkDir -> GetWin10SdkDirHelper -> reg query "%1\Microsoft\Microsoft SDKs\Windows\v10.0" /v "InstallationFolder"
 @rem see winsdk.bat -> GetUniversalCRTSdkDir -> GetUniversalCRTSdkDirHelper -> reg query "%1\Microsoft\Windows Kits\Installed Roots" /v "KitsRoot10"
 
-call "E:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+@rem call "E:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-set NotePadPlusPlusDir="C:\Program Files\Notepad++"
-set NotePadPlusPlusPluginDir=%NotePadPlusPlusDir%\plugins\NppPluginTmpl
+
 set BuildDir=dyzbuild
 set BuildType=Release
+set ProjDir=%cd%
+set ProjName=
+call :get_suf_sub_str %ProjDir% \ ProjName
+echo ProjName %ProjName%
+call :CompileProject %BuildDir% %BuildType% %ProjName%
 
-call :CompileProject %BuildDir% %BuildType%
-
-call :CopyTarget %BuildDir% %BuildType% %NotePadPlusPlusPluginDir%
-
-echo "%NotePadPlusPlusDir%\Notepad++.exe"
-pushd %NotePadPlusPlusDir%
-Notepad++.exe
-popd
 pause
-
 goto :eof
 
 :CompileProject
-	setlocal ENABLEDELAYEDEXPANSION
-	set BuildDir=%~1
-	set BuildType=%2
-	md %BuildDir%
-	pushd %BuildDir%
-		@rem del * /q /s
-		@rem cmake .. -G"Visual Studio 16 2019" -A Win64
-		@rem cmake --build . --target clean
-		cmake .. -DCMAKE_BUILD_TYPE=%BuildType%
-		cmake --build . -j16  --config %BuildType%
-		@rem dir .\examples\helloworld\helloworld.exe
-		@rem .\examples\helloworld\helloworld.exe
-	popd
-	endlocal
+    setlocal ENABLEDELAYEDEXPANSION
+    set BuildDir="%~1"
+    set BuildType="%~2"
+    set ProjName=%~3
+
+    if not exist %BuildDir% (
+        mkdir %BuildDir%
+    )
+    pushd %BuildDir%
+        @rem del * /q /s
+        @rem cmake .. -G"Visual Studio 16 2019" -A Win64
+        @rem cmake --build . --target clean
+        cmake .. -DCMAKE_BUILD_TYPE=%BuildType% -DCMAKE_INSTALL_PREFIX=F:\program\%ProjName%
+        cmake --build . -j16  --config %BuildType% --target INSTALL
+        @rem dir .\examples\helloworld\helloworld.exe
+        @rem .\examples\helloworld\helloworld.exe
+    popd
+    endlocal
 goto :eof
 
 :CopyTarget
-	setlocal ENABLEDELAYEDEXPANSION
-	set BuildDir=%~1
-	set BuildType=%2
-	set NotePadPlusPlusPluginDir=%3
-	for /f %%i in ('dir /s /b "%BuildDir%\bin\%BuildType%\*.dll"') do (   copy %%i %NotePadPlusPlusPluginDir%\ )
-	endlocal
+    setlocal ENABLEDELAYEDEXPANSION
+    set BuildDir=%~1
+    set BuildType=%2
+    set NotePadPlusPlusPluginDir=%3
+    for /f %%i in ('dir /s /b "%BuildDir%\bin\%BuildType%\*.dll"') do (   copy %%i %NotePadPlusPlusPluginDir%\ )
+    endlocal
+goto :eof
+
+:ShowUserInfo
+    echo %date:~6,4%_%date:~0,2%_%date:~3,2%
+    echo %time:~0,2%_%time:~3,2%
+goto :eof
+
+@rem YellowBackground    6f  ef
+@rem BlueBackground      9f  bf   3f
+@rem GreenBackground     af  2f
+@rem RedBackground       4f  cf
+@rem GreyBackground      7f  8f
+@rem PurpleBackground    5f
+
+:color_text
+    setlocal EnableDelayedExpansion
+    for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+        set "DEL=%%a"
+    )
+    echo off
+    <nul set /p ".=%DEL%" > "%~2"
+    findstr /v /a:%1 /R "^$" "%~2" nul
+    del "%~2" > nul 2>&1
+    endlocal
+    echo .
+goto :eof
+
+:get_str_len
+    setlocal ENABLEDELAYEDEXPANSION
+    set mystr=%1
+    set mystrlen=%2
+    set count=0
+    call :color_text 2f "++++++++++++++get_str_len++++++++++++++"
+    :intercept_str_len
+    set /a count+=1
+    for /f %%i in ("%count%") do (
+        if not "!mystr:~%%i,1!"=="" (
+            goto :intercept_str_len
+        )
+    )
+    echo %0 %mystr% %count%
+    endlocal & set %~2=%count%
+goto :eof
+
+:get_char_pos
+    setlocal ENABLEDELAYEDEXPANSION
+    set mystr=%1
+    set char_sym=%2
+    set char_pos=%3
+    call :get_str_len %mystr% mystrlen
+    set count=%mystrlen%
+    call :color_text 2f "++++++++++++++get_char_pos++++++++++++++"
+    :intercept_char_pos
+    set /a count-=1
+    for /f %%i in ("%count%") do (
+        if not "!mystr:~%%i,1!"=="!char_sym!" (
+            goto :intercept_char_pos
+        )
+    )
+    echo %0 %mystr% %char_sym% %count%
+    endlocal & set %~3=%count%
+goto :eof
+
+:get_pre_sub_str
+    setlocal ENABLEDELAYEDEXPANSION
+    set mystr=%1
+    set char_sym=%2
+    set mysubstr=%3
+    call :get_str_len %mystr% mystrlen
+    set count=0
+    call :color_text 2f "++++++++++++++get_pre_sub_str++++++++++++++"
+    set substr=
+    :intercept_pre_sub_str
+    for /f %%i in ("%count%") do (
+        set /a count+=1
+        if not "!mystr:~%%i,1!"=="!char_sym!" (
+            set /a mysubstr_len=%%i
+            set substr=!mystr:~0,%%i!
+            if "%count%" == "%mystrlen%" (
+                goto :pre_sub_str_break
+            )
+            goto :intercept_pre_sub_str
+        ) else (
+            set /a mysubstr_len=%%i
+            set substr=!mystr:~0,%%i!
+            goto :pre_sub_str_break
+        )
+    )
+    :pre_sub_str_break
+    echo %0 %mystr% %char_sym% %count% %mysubstr_len%
+    endlocal & set %~3=%substr%
+goto :eof
+
+:get_suf_sub_str
+    setlocal ENABLEDELAYEDEXPANSION
+    set mystr=%1
+    set char_sym=%2
+    set mysubstr=%3
+    call :get_str_len %mystr% mystrlen
+    set count=%mystrlen%
+    call :color_text 2f "++++++++++++++get_suf_sub_str++++++++++++++"
+    set substr=
+    :intercept_suf_sub_str
+    set /a count-=1
+    for /f %%i in ("%count%") do (
+        if not "!mystr:~%%i,1!"=="!char_sym!" (
+            set /a mysubstr_len=!mystrlen! - %%i
+            set substr=!mystr:~%%i!
+            goto :intercept_suf_sub_str
+        )
+    )
+    echo %0 %mystr% %char_sym% %count% %mysubstr_len%
+    endlocal & set %~3=%substr%
 goto :eof
