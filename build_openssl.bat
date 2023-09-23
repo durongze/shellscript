e5875493e2ea00d1cbf451e27c6de5fa10be9de9
durongze <durongze@qq.com>
2023-09-23 22:08:20 +0800

    Update build_openssl.bat

--- build_openssl.bat
+++ build_openssl.bat
@@ -1,22 +1,38 @@
 set OPENSSL_VERSION=1.0.2d
 set PerlPath=F:\program\Perl\bin
 set NASMPath=F:\program\nasm
+set CMakePath=F:\program\cmake\bin
 
-set OpenSslSrcDir=E:\code\openssl-1.0.2d
-set InstallDir=F:\program\
+set CurDir=%~dp0
+
+set ProjDir=%CurDir:~0,-1%\..
+echo ProjDir %ProjDir%
+
+set OpenSslSrcDir=%CurDir%\openssl-1.0.2d
+set InstallDir=%ProjDir%\out
 set OpenSslBinDir=%InstallDir%\openssl_102d
 
-set VS2013="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
+set VS2005_WIN32="F:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat"
+
+set VS2013_WIN32="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
 set VS2013_AMD64="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\amd64\vcvars64.bat"
-set VS2015="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+
+set VS2015_WIN32="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
 set VS2015_AMD64="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat"
+
+set VS2019_WIN32="E:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+set VS2019_WIN32="E:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+
+set VS2022_WIN32="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
 set VS2022_AMD64="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-set VS2005="F:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat"
+
+set VS2022_WIN32="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+set VS2022_AMD64="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 set path=%NASMPath%;%PerlPath%;%path%
 
 
-@rem CALL %VS2015%
+@rem CALL %VS2015_WIN32%
 @rem CALL %VS2015_AMD64%
 CALL %VS2022_AMD64%
 call :CompileProjectWin64 
