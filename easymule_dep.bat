f30d36f12b269bb7a2014ec8c19cbfdc51c4b0fd
durongze <durongze@qq.com>
2024-08-24 21:51:31 +0800

    sync mule

---
+++ easymule_dep.bat
@@ -0,0 +1,320 @@
+@rem set VSCMD_DEBUG=2
+
+call :DetectVsPath     VisualStudioCmd
+call :DetectProgramDir ProgramDir
+
+echo ProgramDir=%ProgramDir%
+
+
+set tools_addr=https://eternallybored.org/misc/wget/releases/wget-1.21.2-win64.zip
+set tools_addr=%tools_addr%;https://udomain.dl.sourceforge.net/project/gnuwin32/wget/1.11.4-1/wget-1.11.4-1-dep.zip
+set tools_addr=%tools_addr%;https://udomain.dl.sourceforge.net/project/gnuwin32/wget/1.11.4-1/wget-1.11.4-1-bin.zip
+set tools_addr=%tools_addr%;https://udomain.dl.sourceforge.net/project/gnuwin32/tar/1.13-1/tar-1.13-1-bin.zip
+set tools_addr=%tools_addr%;https://udomain.dl.sourceforge.net/project/gnuwin32/unrar/3.4.3/unrar-3.4.3-bin.zip
+set tools_addr=%tools_addr%;https://udomain.dl.sourceforge.net/project/gnuwin32/unzip/5.51-1/unzip-5.51-1-bin.zip
+set tools_addr=%tools_addr%;https://udomain.dl.sourceforge.net/project/gnuwin32/gawk/3.1.6-1/gawk-3.1.6-1-bin.zip
+set tools_addr=%tools_addr%;https://udomain.dl.sourceforge.net/project/gnuwin32/sed/4.2.1/sed-4.2.1-dep.zip
+set tools_addr=%tools_addr%;https://udomain.dl.sourceforge.net/project/gnuwin32/sed/4.2.1/sed-4.2.1-bin.zip
+set tools_addr=%tools_addr%;https://udomain.dl.sourceforge.net/project/gnuwin32/grep/2.5.4/grep-2.5.4-bin.zip
+@rem set tools_addr=%tools_addr%;https://udomain.dl.sourceforge.net/project/gnuwin32/libiconv/1.9.2-1/libiconv-1.9.2-1-bin.zip
+@rem set tools_addr=%tools_addr%;https://udomain.dl.sourceforge.net/project/gnuwin32/libiconv/1.9.2-1/libiconv-1.9.2-1-lib.zip
+@rem set tools_addr=%tools_addr%;https://udomain.dl.sourceforge.net/project/gnuwin32/libiconv/1.9.2-1/libiconv-1.9.2-1-dep.zip
+set tools_addr=%tools_addr%;https://www.7-zip.org/a/lzma2201.7z
+
+
+set software_urls=https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.17.tar.gz
+
+set build_type=Release
+
+set include=%all_inc%;%include%;%tools_dir%\include;
+set lib=%all_lib%;%lib%;%tools_dir%\lib;%tools_dir%\bin;
+set path=%all_bin%;%path%;%tools_dir%\bin;
+
+set PerlPath=%ProgramDir%\Perl\bin
+set NASMPath=%ProgramDir%\nasm\bin
+set CMakePath=%ProgramDir%\cmake\bin
+set PythonHome=%ProgramDir%\python
+set PATH=%NASMPath%;%PerlPath%;%CMakePath%;%PythonHome%;%PATH%
+
+set cur_dir=%~dp0
+set ProjDir=%cur_dir:~0,-1%\..
+echo ProjDir %ProjDir%
+
+set software_dir=%cur_dir%\
+set tools_dir=%cur_dir%\tools_dir
+set home_dir=%ProjDir%\out\windows
+set auto_install_func=%cur_dir%\auto_func.bat
+call %auto_install_func% gen_all_env_by_dir %software_dir% %home_dir% all_inc all_lib all_bin CMAKE_INCLUDE_PATH CMAKE_LIBRARY_PATH CMAKE_MODULE_PATH
+
+@rem set Iconv_LIBRARY=%tools_dir%\bin\libiconv.lib
+
+@rem call %auto_install_func% install_all_package "%tools_addr%" "%tools_dir%"
+@rem call %auto_install_func% install_all_package "%software_urls%" "%software_dir%"
+call :thirdparty_lib_install "%software_dir%" %home_dir%
+pause
+goto :eof
+
+@rem objdump -S E:\program\xz-5.2.6\lib\liblzma.lib | grep -C 5 "lzma_auto_decoder"
+
+:thirdparty_lib_install
+    set lib_dir="%~1"
+    set home_dir="%~2"
+    call :color_text 2f "++++++++++++++thirdparty_lib_install++++++++++++++"
+    pushd %lib_dir%
+        @rem call %auto_install_func% install_package pthread-win32.zip       "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% install_package zlib-1.2.12.tar.gz      "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% install_package capstone-master.zip     "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% install_package SDL-release-2.24.0.zip  "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% install_package unicorn-master.zip      "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% install_package iconv-master.zip        "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "crypto"         "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "crypto51"       "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "crypto55"       "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "zlib"           "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "png"            "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -DPROJECT_IS_TOP_LEVEL=ON  -A Win32"
+        @rem call %auto_install_func% auto_install    "LibJpeg"        "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "CxImage"        "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "id3lib"         "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "json"           "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "ResizableLib"   "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "sqlite3"        "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "TinyXml"        "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "ATL_Server"     "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "WTL91"          "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "WTL91"          "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "updater"          "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "IE2EM"            "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        call %auto_install_func% auto_install    "flag"             "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "Ed2kLoader"       "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+        @rem call %auto_install_func% auto_install    "CrashReporter"    "%home_dir%"  "-DCMAKE_BUILD_TYPE=Release  -A Win32"
+    popd
+goto :eof
+
+
+:DetectProgramDir
+    setlocal EnableDelayedExpansion
+    @rem SkySdk\VS2005\VC
+    set SkySdkDiskSet=C;D;E;F;G;
+    set CurProgramDir=
+    set idx=0
+    call :color_text 2f "+++++++++++++++++++DetectProgramDir+++++++++++++++++++++++"
+    for %%i in (%SkySdkDiskSet%) do (
+        set /a idx+=1
+        for /f "tokens=1-2 delims=|" %%B in ("programs|program") do (
+            set CurProgramDir=%%i:\%%B
+            echo [!idx!] !CurProgramDir!
+            if exist !CurProgramDir!\SkySdk (
+                goto :DetectProgramDirBreak
+            )
+            set CurProgramDir=%%i:\%%C
+            echo [!idx!] !CurProgramDir!
+            if exist !CurProgramDir!\SkySdk (
+                goto :DetectProgramDirBreak
+            )
+        )
+    )
+    :DetectProgramDirBreak
+    set ProgramDir=!CurProgramDir!
+    call :color_text 2f "--------------------DetectProgramDir-----------------------"
+    endlocal & set %~1=%ProgramDir%
+goto :eof
+
+:CheckLibInDir
+    setlocal EnableDelayedExpansion
+    set Libs=%~1
+    set LibDir="%~2"
+    set ProjDir=%~3
+    set MyPlatformSDK=%ProjDir%\lib
+    if not exist "%MyPlatformSDK%" (
+        mkdir %MyPlatformSDK%
+    )
+    call :color_text 2f "+++++++++++++++++++CheckLibInDir+++++++++++++++++++++++"
+    echo LibDir %LibDir%
+    if not exist %LibDir% (
+        call :color_text 4f "--------------------CheckLibInDir-----------------------"
+        goto :eof
+    )
+
+    pushd %LibDir%
+    set idx=0
+    for %%i in (%Libs%) do (
+        set /a idx+=1
+        set CurLib=%%i
+        echo [!idx!] !LibDir!\!CurLib!
+        if not exist !LibDir!\!CurLib! (
+            echo !LibDir!\!CurLib!
+        ) else (
+            copy !LibDir!\!CurLib! %MyPlatformSDK%
+        )
+    )
+    popd
+    call :color_text 2f "--------------------CheckLibInDir-----------------------"
+    endlocal
+goto :eof
+
+:DetectVsPath
+    setlocal EnableDelayedExpansion
+    set VsBatFileVar=%~1
+    set VSDiskSet=C;D;E;F;G;
+    set AllProgramsPathSet=program
+    set AllProgramsPathSet=%AllProgramsPathSet%;programs
+    set AllProgramsPathSet=%AllProgramsPathSet%;"Program Files"
+    set AllProgramsPathSet=%AllProgramsPathSet%;"Program Files (x86)"
+    set VCPathSet=SkySdk\VS2005\VC
+    set VCPathSet=%VCPathSet%;"Microsoft Visual Studio 8\VC"
+    set VCPathSet=%VCPathSet%;"Microsoft Visual Studio 12.0\VC\bin"
+    set VCPathSet=%VCPathSet%;"Microsoft Visual Studio 14.0\VC\bin"
+    set VCPathSet=%VCPathSet%;"Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build"
+    set VCPathSet=%VCPathSet%;"Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build"
+    set VCPathSet=%VCPathSet%;"Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build"
+    set idx_a=0
+    for %%C in (%VCPathSet%) do (
+        set /a idx_a+=1
+        set idx_b=0
+        for %%B in (!AllProgramsPathSet!) do (
+            set /a idx_b+=1
+            set idx_c=0
+            for %%A in (!VSDiskSet!) do (
+                set /a idx_c+=1
+                set CurBatFile=%%A:\%%B\%%C\vcvarsall.bat
+                echo [!idx_a!][!idx_b!][!idx_c!] !CurBatFile!
+                if exist !CurBatFile! (
+                    goto DetectVsPathBreak
+                )
+            )
+        )
+    )
+    :DetectVsPathBreak
+    echo Use:%CurBatFile%
+    call :color_text 2f "--------------------DetectVsPath-----------------------"
+    endlocal & set "%~1=%CurBat%"
+goto :eof
+
+@rem YellowBackground    6f  ef
+@rem BlueBackground      9f  bf   3f
+@rem GreenBackground     af  2f
+@rem RedBackground       4f  cf
+@rem GreyBackground      7f  8f
+@rem PurpleBackground    5f
+
+:color_text
+    setlocal EnableDelayedExpansion
+    for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+        set "DEL=%%a"
+    )
+    echo off
+    <nul set /p ".=%DEL%" > "%~2"
+    findstr /v /a:%1 /R "^$" "%~2" nul
+    del "%~2" > nul 2>&1
+    endlocal
+    echo .
+goto :eof
+
+:get_str_len
+    setlocal ENABLEDELAYEDEXPANSION
+    set mystr=%~1
+    set mystrlen="%~2"
+    set count=0
+    call :color_text 2f "++++++++++++++get_str_len++++++++++++++"
+    :intercept_str_len
+    set /a count+=1
+    for /f %%i in ("%count%") do (
+        if not "!mystr:~%%i,1!"=="" (
+            goto :intercept_str_len
+        )
+    )
+    echo %0 %mystr% %count%
+    endlocal & set %~2=%count%
+goto :eof
+
+:get_first_char_pos
+    setlocal ENABLEDELAYEDEXPANSION
+    set mystr=%~1
+    set char_sym=%~2
+    set char_pos="%~3"
+    call :get_str_len %mystr% mystrlen
+    set count=0
+    call :color_text 2f "++++++++++++++get_first_char_pos++++++++++++++"
+    :intercept_first_char_pos
+    for /f %%i in ("%count%") do (
+        set /a count+=1	
+        if not "!mystr:~%%i,1!"=="!char_sym!" (
+            goto :intercept_first_char_pos
+        )
+    )
+    echo %0 %mystr% %char_sym% %count%
+    endlocal & set %~3=%count%
+goto :eof
+
+:get_last_char_pos
+    setlocal ENABLEDELAYEDEXPANSION
+    set mystr=%~1
+    set char_sym=%~2
+    set char_pos="%~3"
+    call :get_str_len %mystr% mystrlen
+    set count=%mystrlen%
+    call :color_text 2f "++++++++++++++get_last_char_pos++++++++++++++"
+    @rem set /a count-=1	
+    :intercept_last_char_pos
+    for /f %%i in ("%count%") do (
+        if not "!mystr:~%%i,1!"=="!char_sym!" (
+            set /a count-=1			
+            goto :intercept_last_char_pos
+        )
+    )
+    echo %0 %mystr% %char_sym% %count%
+    endlocal & set %~3=%count%
+goto :eof
+
+:get_pre_sub_str
+    setlocal ENABLEDELAYEDEXPANSION
+    set mystr=%~1
+    set char_sym=%~2
+    set mysubstr="%~3"
+    call :get_str_len %mystr% mystrlen
+    set count=0
+    call :color_text 2f "++++++++++++++get_pre_sub_str++++++++++++++"
+    set substr=
+    :intercept_pre_sub_str
+    for /f %%i in ("%count%") do (
+        set /a count+=1
+        if not "!mystr:~%%i,1!"=="!char_sym!" (
+            set /a mysubstr_len=%%i
+            set substr=!mystr:~0,%%i!
+            if "%count%" == "%mystrlen%" (
+                goto :pre_sub_str_break
+            )
+            goto :intercept_pre_sub_str
+        ) else (
+            set /a mysubstr_len=%%i
+            set substr=!mystr:~0,%%i!
+            goto :pre_sub_str_break
+        )
+    )
+    :pre_sub_str_break
+    echo %0 %mystr% %char_sym% %count% %mysubstr_len%
+    endlocal & set %~3=%substr%
+goto :eof
+
+:get_suf_sub_str
+    setlocal ENABLEDELAYEDEXPANSION
+    set mystr=%~1
+    set char_sym=%~2
+    set mysubstr="%~3"
+    call :get_str_len %mystr% mystrlen
+    set count=%mystrlen%
+    call :color_text 2f "++++++++++++++get_suf_sub_str++++++++++++++"
+    set substr=
+    :intercept_suf_sub_str
+    for /f %%i in ("%count%") do (
+        if not "!mystr:~%%i,1!"=="!char_sym!" (
+            set /a mysubstr_len=!mystrlen! - %%i
+            set substr=!mystr:~%%i!
+            set /a count-=1	
+            goto :intercept_suf_sub_str
+        )
+    )
+    echo %0 %mystr% %char_sym% %count% %mysubstr_len%
+    endlocal & set %~3=%substr%
+goto :eof
+
