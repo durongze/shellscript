68ab9e15fc5e2cce73fccd965f37e9da6e2f9508
durongze <durongze@qq.com>
2025-03-14 00:11:43 +0800

    build

--- DetectVs.bat
+++ DetectVs.bat
@@ -1,4 +1,9 @@
-call :DetectVsPath VisualStudioCmd
+@rem set VSCMD_DEBUG=2
+
+call :DetectVsPath     VisualStudioCmd
+call :DetectProgramDir ProgramDir
+
+echo ProgramDir=%ProgramDir%
 
 @rem copy compiler\mr_helper.mrp compiler\mr_helpere.mrp
 
@@ -6,13 +11,13 @@ set CurDir=%~dp0
 set ProjDir=%CurDir:~0,-1%
 set MrpHomeDir=%ProjDir%\..\..\
 
-set ProgramDir=F:\program
-
 set MythSdkDir=%MrpHomeDir%\mythsdk
 set MythSdkVmDir=%MrpHomeDir%\vmrp\BuildLib\Debug
 set MythSdkDemoDir=%ProjDir%
 
 set SkySdkDir=%ProgramDir%\SkySdk
+set SkySdkIncDir=%SkySdkDir%\include
+set SkySdkLibDir=%SkySdkDir%\Simulator\lib
 set SkySdkVmDir=%SkySdkDir%\MrpSimulator\
 set SkySdkDemoDir=%SkySdkDir%\SkyGuide\Templates\2052
 
@@ -20,11 +25,15 @@ set MySdkDir=%cd%\..\..\..\..\
 set MySdkVmDir=%MySdkDir%\MrpSimulator
 set MySdkDemoDir=%cd%\mrp
 
+set PERL5LIB=%PERL5LIB%
 set PerlPath=%ProgramDir%\Perl\bin
-set NASMPath=%ProgramDir%\nasm
+set NASMPath=%ProgramDir%\nasm\bin
+set YASMPath=%ProgramDir%\yasm\bin
+set GPERFPath=%ProgramDir%\gperf\bin
 set CMakePath=%ProgramDir%\cmake\bin
+set MakePath=%ProgramDir%\make-3.81-bin\bin
 set PythonHome=%ProgramDir%\python
-set PATH=%NASMPath%;%PerlPath%;%CMakePath%;%PythonHome%;%PATH%
+set PATH=%NASMPath%;%YASMPath%;%GPERFPath%;%PerlPath%;%CMakePath%;%SDCCPath%;%MakePath%;%PythonHome%;%PythonHome%\Scripts;%PATH%
 
 set HomeDir=%MrpHomeDir%\vmrp\out\windows
 
@@ -35,32 +44,160 @@ set ARMINC=%ARMHOME%\include
 set ARMLIB=%ARMHOME%\lib
 set path=%ArmHome%\bin;%path%
 
-set VCInstallDir=F:\Program Files (x86)\Microsoft Visual Studio 8\VC
-set VscLib=msvcmrtd.lib;msvcrtd.lib;
-set VscDir=%VCInstallDir%\lib
-set VsAtlmfcLib=mfcs80d.lib atlsd.lib mfc80d.lib
-set VsAtlmfcDir=%VCInstallDir%\atlmfc\lib
-set FrameworkLib=kernel32.lib;user32.lib;gdi32.lib;winspool.lib;shell32.lib;ole32.lib;oleaut32.lib;uuid.lib;comdlg32.lib;advapi32.lib;WS2_32.lib;winmm.lib;vfw32.lib;
-set FrameworkDir=%VCInstallDir%\PlatformSDK\lib
-set SkySdkLib=dsound.lib;dxguid.lib;simulator.lib;simlib.lib;jpeg_sim.lib;SIM_mr_helperexb.lib;data_codec_sim.lib;SIM_mr_helperexbnp.lib;
-
-call %VisualStudioCmd%
-
-call :ShowVS2022InfoOnWin10
-
-@rem call :CheckLibInDir         "%VscLib%"        "%WindowsSdkDir%"
-call :CheckLibInDir         "%VscLib%"             "%VscDir%"
-call :CheckLibInDir         "%VsAtlmfcLib%"        "%VsAtlmfcDir%"
-call :CheckLibInDir         "%FrameworkLib%"       "%FrameworkDir%"
-
-@rem call :CheckLibInDir         "%SkySdkLib%"     "%SkySdkDir%\Simulator\lib"
+@rem x86  or x64
+call %VisualStudioCmd% x86
 pause
+
+@rem call "C:\Qt\6.5.2\msvc2019_64\bin\qtenv2.bat"
+@rem call "D:/Qt/Qt5.12.0/5.12.0/msvc2017_64/bin/qtenv2.bat"
+pushd %CurDir%
+
+@rem Win32  or x64
+set ArchType=Win32
+
+call :CopyVS2005Libs         "%ProjDir%"
+@rem call :ShowVS2022InfoOnWin10
+
+pause
+goto :eof
+
+:DetectProgramDir
+    setlocal EnableDelayedExpansion
+    @rem SkySdk\VS2005\VC
+    set SkySdkDiskSet=C;D;E;F;G;
+    set CurProgramDir=
+    set idx=0
+    call :color_text 2f " +++++++++++++++++++ DetectProgramDir +++++++++++++++++++++++ "
+    for %%i in (%SkySdkDiskSet%) do (
+        set /a idx+=1
+        for /f "tokens=1-2 delims=|" %%B in ("programs|program") do (
+            set CurProgramDir=%%i:\%%B
+            echo [!idx!] !CurProgramDir!
+            if exist !CurProgramDir!\SkySdk (
+                goto :DetectProgramDirBreak
+            )
+            set CurProgramDir=%%i:\%%C
+            echo [!idx!] !CurProgramDir!
+            if exist !CurProgramDir!\SkySdk (
+                goto :DetectProgramDirBreak
+            )
+        )
+    )
+    :DetectProgramDirBreak
+    set ProgramDir=!CurProgramDir!
+    call :color_text 2f " ------------------- DetectProgramDir ----------------------- "
+    endlocal & set %~1=%ProgramDir%
+goto :eof
+
+:GenSkySdkEnvVars
+    setlocal EnableDelayedExpansion
+    set SkySdkDiskSet=C;D;E;F;G;
+    call :color_text 2f " +++++++++++++++++++ GenSkySdkEnvVars +++++++++++++++++++++++ "
+    call :DetectProgramDir ProgramDir
+    call :color_text 2f " ------------------- GenSkySdkEnvVars ---------------------- "
+    set SkySdkDir=%ProgramDir%\SkySdk
+    set SkySdkIncDir=%SkySdkDir%\include\
+    set SkySdkLibDir=%SkySdkDir%\Simulator\lib\
+    set SkySdkLibFiles=dsound.lib dxguid.lib simulator.lib simlib.lib jpeg_sim.lib SIM_mr_helperexb.lib data_codec_sim.lib SIM_mr_helperexbnp.lib 
+    endlocal & set %~1=%SkySdkIncDir%& set %~2=%SkySdkLibDir%& set %~3=%SkySdkLibFiles%
+goto :eof
+
+:GenVsIdeEnvVars
+    setlocal EnableDelayedExpansion
+    call :color_text 2f " +++++++++++++++++++ GenVsIdeEnvVars +++++++++++++++++++++++ "
+    call :DetectVS2005VcDir   VCInstallDir
+    set VsCppLib=msvcmrtd.lib msvcrtd.lib
+    set VsCppDir=%VCInstallDir%\lib
+    set VsAtlmfcLib=mfcs80d.lib atlsd.lib mfc80d.lib
+    set VsAtlmfcDir=%VCInstallDir%\atlmfc\lib
+    set FrameworkLib=kernel32.lib user32.lib gdi32.lib winspool.lib shell32.lib ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib WS2_32.lib winmm.lib vfw32.lib 
+    set FrameworkDir=%VCInstallDir%\PlatformSDK\lib
+    call :color_text 2f " -------------------- GenVsIdeEnvVars ----------------------- "
+    endlocal & set "%~1=%VsCppLib%"& set "%~2=%VsAtlmfcLib%"& set "%~3=%FrameworkLib%"& set "%~4=%VsCppDir%"& set "%~5=%VsAtlmfcDir%"& set "%~6=%FrameworkDir%"
+goto :eof
+
+:DetectVS2005VcDir
+    setlocal EnableDelayedExpansion
+    set OutVCInstallDir=%~1
+
+    set CurVcDir=
+    set idx=0
+    call :color_text 2f " +++++++++++++++++++ DetectVS2005VcDir +++++++++++++++++++++++ "
+    set VSDiskSet=C;D;E;F;G;
+
+    set AllProgramsPathSet="program"
+    set AllProgramsPathSet=%AllProgramsPathSet%;"programs"
+    set AllProgramsPathSet=%AllProgramsPathSet%;"Program Files"
+    set AllProgramsPathSet=%AllProgramsPathSet%;"Program Files (x86)"
+
+    set VCPathSet=%VCPathSet%;"SkySdk\VS2005\VC"
+    set VCPathSet=%VCPathSet%;"Microsoft Visual Studio 8\VC"
+
+    set idx_a=0
+    for %%C in (%VCPathSet%) do (
+        set /a idx_a+=1
+        set idx_b=0
+        for %%B in (!AllProgramsPathSet!) do (
+            set /a idx_b+=1
+            set idx_c=0
+            for %%A in (!VSDiskSet!) do (
+                set /a idx_c+=1
+                set CurVcDir=%%A:\%%~B\%%~C
+                echo [!idx!] VS80COMNTOOLS=!VS80COMNTOOLS!
+                echo [!idx!] CurVcDir=!CurVcDir!
+                if exist !CurVcDir! (
+                    goto :DetectVS2005VcDirBreak
+                )
+            )
+        )
+    )
+    :DetectVS2005VcDirBreak
+    set  VCInstallDir=!CurVcDir!
+    echo VCInstallDir=!CurVcDir!
+    call :color_text 2f " -------------------- DetectVS2005VcDir ----------------------- "
+    endlocal & set "%~1=%VCInstallDir%"
+goto :eof
+
+:CopyVS2005Libs
+    setlocal EnableDelayedExpansion
+    set ProjDir=%~1
+    call :color_text 2f " +++++++++++++++++++ CopyVS2005Libs +++++++++++++++++++++++ "
+    @rem call :DetectVS2005VcDir   VCInstallDir
+    call :DetectProgramDir    ProgramDir
+    set VCInstallDir=%ProgramDir%\SkySdk\VS2005\VC
+
+    set VscLib=msvcmrtd.lib;msvcrtd.lib;
+    set VscDir=%VCInstallDir%\lib
+
+    set VsAtlmfcLib=mfcs80d.lib atlsd.lib mfc80d.lib
+    set VsAtlmfcDir=%VCInstallDir%\atlmfc\lib
+
+    set FrameworkLib=kernel32.lib;user32.lib;gdi32.lib;winspool.lib;shell32.lib;ole32.lib;oleaut32.lib;uuid.lib;comdlg32.lib;advapi32.lib;WS2_32.lib;winmm.lib;vfw32.lib;
+    set FrameworkDir=%VCInstallDir%\PlatformSDK\lib
+
+    set RedistDir=%VCInstallDir%\redist\Debug_NonRedist\x86
+
+    set SkySdkLib=dsound.lib;dxguid.lib;simulator.lib;simlib.lib;jpeg_sim.lib;SIM_mr_helperexb.lib;data_codec_sim.lib;SIM_mr_helperexbnp.lib;
+
+    call :CheckLibInDir         "%VscLib%"             "%VscDir%"         "%ProjDir%"
+    call :CheckLibInDir         "%VsAtlmfcLib%"        "%VsAtlmfcDir%"    "%ProjDir%"
+    @rem call :CheckLibInDir         "%FrameworkLib%"       "%FrameworkDir%"                 "%ProjDir%"
+
+    mkdir %ProjDir%\out\lib
+    xcopy %RedistDir%         "%ProjDir%\out\"         /y /s /e   
+    xcopy %RedistDir%         "%ProjDir%\out\lib\"     /y /s /e   
+
+    @rem call :CheckLibInDir         "%SkySdkLib%"          "%SkySdkDir%\Simulator\lib"      "%ProjDir%"
+
+    call :color_text 2f " -------------------- CopyVS2005Libs ----------------------- "
+    endlocal
 goto :eof
 
 :CheckLibInDir
     setlocal EnableDelayedExpansion
     set Libs=%~1
     set LibDir="%~2"
+    set ProjDir=%~3
     set MyPlatformSDK=%ProjDir%\lib
     if not exist "%MyPlatformSDK%" (
         mkdir %MyPlatformSDK%
@@ -69,6 +206,7 @@ goto :eof
     echo LibDir %LibDir%
     if not exist %LibDir% (
         call :color_text 4f " -------------------- CheckLibInDir ----------------------- "
+        echo '%LibDir%' does not exist... 
         goto :eof
     )
 
@@ -124,7 +262,8 @@ goto :eof
     set VCPathSet=%VCPathSet%;"Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build"
     set VCPathSet=%VCPathSet%;"Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build"
     set VCPathSet=%VCPathSet%;"Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build"
-    set VCPathSet=%VCPathSet%;SkySdk\VS2005\VC
+    set VCPathSet=%VCPathSet%;"VS2022\VC\Auxiliary\Build"
+    set VCPathSet=%VCPathSet%;"SkySdk\VS2005\VC"
     set VCPathSet=%VCPathSet%;"Microsoft Visual Studio 8\VC"
     set VCPathSet=%VCPathSet%;"Microsoft Visual Studio 12.0\VC\bin"
     set VCPathSet=%VCPathSet%;"Microsoft Visual Studio 14.0\VC\bin"
