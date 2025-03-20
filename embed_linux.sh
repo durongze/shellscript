75ce2467646a5725c859464eab81d24086b33bb0
root <root@SNSVP11.>
2025-03-20 21:42:45 +0800

    func

--- embed_linux.sh
+++ embed_linux.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #u-boot-2020.01/arch/arm/dts/BOARD_NAME.dts
 make BOARD_NAME_defconfig
 make DEVICE_TREE=BOARD_NAME all -j8
@@ -9,12 +11,14 @@ make uImage dtbs LOADADDR=0XC2000040 -j8
 #busybox-1.32.0/configs/BOARD_NAME_defconfig
 make BOARD_NAME_defconfig
 
-sudo apt install python-is-python3
-sudo apt install lbzip2
-sudo apt install bison
-sudo apt install flex 
-sudo apt install libncurses-dev
-sudo apt install libssl-dev
-sudo apt install mkimage
-sudo apt install u-boot-tools
-
+function InstallCompileTools()
+{
+    sudo apt install python-is-python3
+    sudo apt install lbzip2
+    sudo apt install bison
+    sudo apt install flex 
+    sudo apt install libncurses-dev
+    sudo apt install libssl-dev
+    sudo apt install mkimage
+    sudo apt install u-boot-tools
+}
